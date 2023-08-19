7b4f454987f36ad253f32cbcd0a2cb8709acca3d
Claude Stephan M. Kouame <claudestephane2010@live.fr>
2023-08-19 19:28:38 +0200

    Commit before vacancy. please not compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
-// Test code for Ultimate GPS Using Hardware Serial (e.g. GPS Flora or FeatherWing)
-//
-// This code shows how to listen to the GPS module via polling. Best used with
-// Feathers or Flora where you have hardware Serial and no interrupt
-//
-// Tested and works great with the Adafruit GPS FeatherWing
-// ------> https://www.adafruit.com/products/3133
-// or Flora GPS
-// ------> https://www.adafruit.com/products/1059
-// but also works with the shield, breakout
-// ------> https://www.adafruit.com/products/1272
-// ------> https://www.adafruit.com/products/746
-//
-// Pick one up today at the Adafruit electronics shop
-// and help support open source hardware & software! -ada
 
 #include "driver/gps.h"
 #include "driver/piezo.h"
+
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
 
 void setup()
 {
