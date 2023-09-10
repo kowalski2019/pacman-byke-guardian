d3eb69d2a3afb6c694dbeb95c2410c608df8256e
Claude Stephan M. Kouame <claudestephane2010@live.fr>
2023-09-10 21:50:10 +0200

    State machine and Accelerometer updated

--- src/driver/accelerometer.h
+++ src/driver/accelerometer.h
@@ -1,7 +1,12 @@
 #ifndef _ACC_H
 #define _ACC_H
 
+#include <stdint.h>
+
 typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
     float x_speed;
     float y_speed;
     float z_speed;
